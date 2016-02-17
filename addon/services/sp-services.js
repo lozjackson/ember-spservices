0e328af5b2d8ff88beda67536c3c53856543a04e
Loz Jackson <lozjackson@talktalk.net>
2016-02-17 22:29:55 +0000

    update sp-services service

--- addon/services/sp-services.js
+++ addon/services/sp-services.js
@@ -1,4 +1,124 @@
+/**
+  @module ember-spservices
+*/
 import Ember from 'ember';
+import FieldVersionObject from 'ember-spservices/objects/field-version';
 
+/**
+  ## SpServices Service
+
+  This is a wrapper around the jQuery.SPServices library.
+
+  https://github.com/sympmarc/SPServices
+
+  @class SpServicesService
+  @namespace Services
+*/
 export default Ember.Service.extend({
+
+  /**
+    The `SPServices` library.
+
+    @property _SPServices
+    @type {Object}
+    @private
+  */
+  _SPServices: Ember.$().SPServices,
+
+  /**
+    ## GetCurrentUser
+
+    Get the current user using the `SPServices.SPGetCurrentUser` method.
+    An `Ember.Object` is returned with camelized field names as keys.
+
+    You can pass in an array of field names to be collected.
+
+    ```
+    let currentUser = spServices.getCurrentUser(["ID", "Name"]);
+    currentUser.get('id') // ID
+    currentUser.get('name') // Name
+    ```
+
+    If you don't pass in an array of field names then the default set will be collected.
+
+    The default set of fields are: `['ID', 'Name', 'Title', 'EMail']`
+
+    @method getCurrentUser
+    @param {Array} fieldNames
+    @return {Object}
+  */
+  getCurrentUser(fieldNames) {
+    let currentUser;
+    let hash = {};
+    let _SPServices = this.get('_SPServices');
+    if (!_SPServices) { return; }
+    currentUser = _SPServices.SPGetCurrentUser({
+      fieldNames: fieldNames || ['ID', 'Name', 'Title', 'EMail']
+    });
+    Object.keys(currentUser).forEach(function (key) {
+      hash[ (key === 'ID') ? 'id' : Ember.String.camelize(key) ] = currentUser[key];
+    });
+    return Ember.Object.create(hash);
+  },
+
+  /**
+    ## GetVersionCollection
+
+    Call the `GetVersionCollection` operation.  You can pass in an array which
+    will be populated with `FieldVersionObjects`, or you can pass in a callback
+    function.
+
+    An example, passing in an array.
+
+    ```
+    // an array to store the version collection in
+    let versionCollection = Ember.A();
+
+    spServices.getVersionCollection(listName, itemId, fieldName, versionCollection);
+    ```
+
+    The version collection will then be stored in the `versionCollection` property.
+
+    Alternatively you can pass in a callback function.
+
+    ```
+    let callback = function (xData, status) {
+      //...
+    };
+
+    spServices.getVersionCollection(listName, itemId, fieldName, callback);
+    ```
+
+    @method getVersionCollection
+    @param {String} strlistID
+    @param {String} strlistItemID
+    @param {String} strFieldName
+    @param {Array|Function} results
+  */
+  getVersionCollection(strlistID, strlistItemID, strFieldName, results) {
+    let _SPServices = this.get('_SPServices');
+    if (!_SPServices || !strlistID || !strlistItemID || !strFieldName) { return; }
+    _SPServices({
+      operation: "GetVersionCollection",
+      async: true,
+      strlistID: strlistID,
+      strlistItemID: strlistItemID,
+      strFieldName: strFieldName,
+      completefunc: function (xData , status) {
+        if (typeof results !== 'undefined') {
+          if (typeof results === 'function') {
+            results.call(this, xData, status);
+          } else {
+            Ember.$(xData.responseText).find("Version").each(function(/*i*/) {
+              results.pushObject(FieldVersionObject.create({
+                value: Ember.$(this).attr(strFieldName),
+                modified: Ember.$(this).attr("Modified"),
+                editor: Ember.$(this).attr("Editor")
+              }));
+            });
+          }
+        }
+      }
+    });
+  }
 });
--- tests/unit/services/sp-services-test.js
+++ tests/unit/services/sp-services-test.js
@@ -1,13 +1,113 @@
 import { moduleFor, test } from 'ember-qunit';
+import Ember from 'ember';
 
 moduleFor('service:sp-services', 'Unit | Service | sp services', {
-  // Specify the other units that are required for this test.
-  // needs: ['service:foo']
 });
 
-// Replace this with your real tests.
 test('it exists', function(assert) {
   var service = this.subject();
+  assert.ok(service);
+});
 
+test('getCurrentUser() method', function(assert) {
+  assert.expect(10);
+  var service = this.subject();
+  service.set('_SPServices', {
+    SPGetCurrentUser: (obj) => {
+      assert.equal(obj.fieldNames.length, 4);
+      assert.equal(obj.fieldNames[0], 'ID');
+      assert.equal(obj.fieldNames[1], 'Name');
+      assert.equal(obj.fieldNames[2], 'Title');
+      assert.equal(obj.fieldNames[3], 'EMail');
+      return {
+        ID: 10,
+        Name: `Domain\Test.User`,
+        Title: 'Test User',
+				EMail: 'test.user@example.com'
+			};
+    }
+  });
+
+  let user = service.getCurrentUser();
+  assert.ok(service);
+  assert.equal(user.get('id'), 10);
+  assert.equal(user.get('name'), `Domain\Test.User`);
+  assert.equal(user.get('title'), 'Test User');
+  assert.equal(user.get('eMail'), 'test.user@example.com');
+});
+
+test('getCurrentUser() method - pass in fieldNames', function(assert) {
+  assert.expect(6);
+  var service = this.subject();
+  service.set('_SPServices', {
+    SPGetCurrentUser: (obj) => {
+      assert.equal(obj.fieldNames.length, 2);
+      assert.equal(obj.fieldNames[0], 'ID');
+      assert.equal(obj.fieldNames[1], 'Name');
+      return {
+        ID: 10,
+        Name: 'Test User'
+			};
+    }
+  });
+
+  let user = service.getCurrentUser(["ID", "Name"]);
+  assert.ok(service);
+  assert.equal(user.get('id'), 10);
+  assert.equal(user.get('name'), 'Test User');
+});
+
+test('getVersionCollection() method - pas in an array', function(assert) {
+  assert.expect(8);
+  var service = this.subject();
+  let listName = 'testList';
+  let itemId = 1;
+  let fieldName = 'fieldName';
+  let versionCollection = Ember.A();
+  let xml = `<Version>
+    <Comment>Abc</Comment>
+    <Modified>2015-11-13T10:21:02Z</Modified>
+    <Editor>11;#Test User,#DOMAIN\test.user,#test.user@example.com,#,#Test User</Editor>
+  </Version>`;
+
+  service.set('_SPServices', (obj) => {
+    assert.equal(obj.operation, 'GetVersionCollection');
+    assert.equal(obj.async, true);
+    assert.equal(obj.strlistID, listName);
+    assert.equal(obj.strlistItemID, itemId);
+    assert.equal(obj.strFieldName, fieldName);
+    assert.equal(typeof obj.completefunc, 'function');
+
+    obj.completefunc({
+      responseText: Ember.$.parseXML(xml)
+    });
+  });
+  service.getVersionCollection(listName, itemId, fieldName, versionCollection);
+  assert.ok(service);
+  assert.equal(versionCollection.length, 1, `should be 1 version`);
+});
+
+test('getVersionCollection() method - pass in a function', function(assert) {
+  assert.expect(9);
+  var service = this.subject();
+  let listName = 'testList';
+  let itemId = 1;
+  let fieldName = 'fieldName';
+  let callback = (xData, status) => {
+    assert.equal(xData, 'data');
+    assert.equal(status, 'status');
+  };
+
+  service.set('_SPServices', (obj) => {
+    assert.equal(obj.operation, 'GetVersionCollection');
+    assert.equal(obj.async, true);
+    assert.equal(obj.strlistID, listName);
+    assert.equal(obj.strlistItemID, itemId);
+    assert.equal(obj.strFieldName, fieldName);
+    assert.equal(typeof obj.completefunc, 'function');
+
+    obj.completefunc('data', 'status');
+  });
+  service.getVersionCollection(listName, itemId, fieldName, callback);
   assert.ok(service);
 });
